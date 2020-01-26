3a5a01bcf47103f3568be496a666a53b9e8f9b63
aloks <srialok01@gmail.com>
2020-01-26 21:34:35 +0530

    first commit

--- Util/practrise.py
+++
@@ -1,9 +0,0 @@
-
-from selenium import webdriver
-driver = webdriver.Chrome(executable_path="C:/Users/aloks/PycharmProjects/Automation Projects/Drivers/chromedriver.exe")
-
-driver.get('https://demo.nopcommerce.com/onepagecheckout')
-abc =driver.title
-
-print(abc)
-driver.quit()
